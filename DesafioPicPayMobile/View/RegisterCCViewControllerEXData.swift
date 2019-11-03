94ecd4fd47f5a74db96a826ca10d2b63cbdff73d
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-03 15:18:28 -0300

    Create function to save data and check data on user defaults

--- DesafioPicPayMobile/View/RegisterCCViewControllerEXData.swift
+++ DesafioPicPayMobile/View/RegisterCCViewControllerEXData.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 Kelvin Batista Machado. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension RegisterCCViewController {
+    
+    // Save Data
+    func saveData() {
+        defaults.set(numCC.text!, forKey: Keys.card_number)
+        defaults.set(holderName.text!, forKey: Keys.holder_name)
+        defaults.set(expirationDate.text!, forKey: Keys.expiry_date)
+        defaults.set(cvv.text!, forKey: Keys.cvv)
+        RegisterCCViewController.existeCartao = true
+    }
+
+    // Check Save Data
+     func checkForSavedData() {
+        let card_number = defaults.value(forKey: Keys.card_number) as? String ?? ""
+        numCC.text = card_number
+        let holder_name = defaults.value(forKey: Keys.holder_name) as? String ?? ""
+        holderName.text = holder_name
+        let expiry_date = defaults.value(forKey: Keys.expiry_date) as? String ?? ""
+        expirationDate.text = expiry_date
+        let checkCvv = defaults.value(forKey: Keys.cvv) as? String ?? ""
+        cvv.text = checkCvv
+        
+        RegisterCCViewController.existeCartao = true
+        
+        if  numCC.text == "" || holderName.text == "" || expiry_date == "" || cvv.text == "" {
+            RegisterCCViewController.existeCartao = false
+        }
+    }
+
+}
