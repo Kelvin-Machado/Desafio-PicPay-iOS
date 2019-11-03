0e1d28497494288d520daf339818ba71de79c81a
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-03 14:14:54 -0300

    added ContactsTableViewController

---
+++ DesafioPicPayMobile/View/ContactsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactTableViewController.swift
+//  DesafioPicPayMobile
+//
+//  Created by Kelvin Batista Machado on 03/11/19.
+//  Copyright © 2019 Kelvin Batista Machado. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewController: UITableViewController, UISearchBarDelegate {
+
+}
