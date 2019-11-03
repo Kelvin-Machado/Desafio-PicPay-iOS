6a3f7bd1743f073d66e5207894ec93bd53703066
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-03 15:10:51 -0300

    Configure header settings

--- DesafioPicPayMobile/View/ViewController.swift
+++ DesafioPicPayMobile/View/ViewController.swift
@@ -19,6 +19,26 @@ class ViewController: UIViewController {
         
         self.addChild(contactsTable)
         view.addSubview(contactsTable.tableView)
+        
+        setupHeader()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        contactsTable.tableView.reloadData()
+    }
+    
+    func setupHeader() {
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Contatos")
+        imageView.contentMode = .topLeft
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 300)
+        view.addSubview(imageView)
+        
+        contactsTable.imageView = imageView
     }
 
 
