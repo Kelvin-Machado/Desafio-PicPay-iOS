3150f0241e3bbb823be11026d4c567c143993c1e
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-04 10:53:14 -0300

    Create a struct to save user defaults keys

---
+++ DesafioPicPayMobile/Model/CreditCardData.swift
@@ -0,0 +1,16 @@
+//
+//  CreditCardData.swift
+//  DesafioPicPayMobile
+//
+//  Created by Kelvin Batista Machado on 04/11/19.
+//  Copyright © 2019 Kelvin Batista Machado. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    static let card_number  = "card_number"
+    static let cvv = "cvv"
+    static let expiry_date  = "expiry_date"
+    static let holder_name = "holder_name"
+}
