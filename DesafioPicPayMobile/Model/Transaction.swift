87725b79b2bf379c1032380db2657f7da7c44094
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-05 20:11:03 -0300

    Add a Struct to save post request data

---
+++ DesafioPicPayMobile/Model/Transaction.swift
@@ -0,0 +1,13 @@
+//
+//  Transaction.swift
+//  DesafioPicPayMobile
+//
+//  Created by Kelvin Batista Machado on 05/11/19.
+//  Copyright © 2019 Kelvin Batista Machado. All rights reserved.
+//
+
+import Foundation
+//
+//struct TransactionResponse: Decodable{
+//    
+//}
